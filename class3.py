4980a769acd5a8815ac253968b85cfa77a87fd13
Alec Muffett <frstrtr@gmail.com>
2019-08-29 18:01:51 +0300

    Solution

--- class3.py
+++ class3.py
@@ -1,3 +1,5 @@
+import functools
+
 """
     HomeTask for 29.08.19
 """
@@ -26,7 +28,7 @@ print(a)
 # Take a list
 # a=[1,3,5,7,9,11,13,15,17,19]
 # a=[0.1, 12, 34, 0.02, 10.5, 9.9999999999, 10.00000000000000000001, 7, 4]
-# a=[‘a’, 10, 13, ‘10’, ‘ten’, 9, 9, 11, 11]
+# a=['a', 10, 13, '10', 'ten', 9, 9, 11, 11]
 # and write a program that prints out all the elements of the list that are less than 10.
 
 a = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
@@ -48,19 +50,50 @@ for element in a:
 
 # Write a program that takes a list
 # a=[12, 13, 13, 14, 14.5, 17, 17]
-# a=[‘1’, 1, 0.1, 1, ‘01’, ‘1’, 10, ‘010’, ‘10’, [10, 20], [20, 10], 1, [10, 20], [‘20’, ‘10’]]
+# a=['1', 1, 0.1, 1, '01', '1', 10, '010', '10', [10, 20], [20, 10], 1, [10, 20], ['20', '10']]
 # a=[-78, 60, -24, -77, 47, 20, 78, -73, 63, -56, 20, 89, 66, -44, 28, 64, 96, 60, 21, -42, 92, -7, -53, 42, -2, 37, -85, 32, -57, -35, -93, -30, -36, -78, 8, -47, -32, 28, 50, 44, -76, -63, 80, 47, 21, -90, 48, 9, -61, 26, -11, -89, 84, 4, -39, -82, -80, -27, -15, -8, 5, 62, -79, 19, 34, -30, 11, 72, 82, -72, -49, -75, -71, -31, 24, 36, -33, -1, -95, -78, -9, 96, 88, 82, 77, 93, -96, -52, 80, 77, 95, 57, -48, 14, -40, -81, -81, 97, 29, -27]
 # and returns a new list that contains all the elements of the first list minus all the duplicates.
 
-a=[12, 13, 13, 14, 14.5, 17, 17]
-a=['1', 1, 0.1, 1, '01', '1', 10, '010', '10', [10, 20], [20, 10], 1, [10, 20], ['20', '10']]
-a=[-78, 60, -24, -77, 47, 20, 78, -73, 63, -56, 20, 89, 66, -44, 28, 64, 96, 60, 21, -42, 92, -7, -53, 42, -2, 37, -85, 32, -57, -35, -93, -30, -36, -78, 8, -47, -32, 28, 50, 44, -76, -63, 80, 47, 21, -90, 48, 9, -61, 26, -11, -89, 84, 4, -39, -82, -80, -27, -15, -8, 5, 62, -79, 19, 34, -30, 11, 72, 82, -72, -49, -75, -71, -31, 24, 36, -33, -1, -95, -78, -9, 96, 88, 82, 77, 93, -96, -52, 80, 77, 95, 57, -48, 14, -40, -81, -81, 97, 29, -27]
+a = [12, 13, 13, 14, 14.5, 17, 17]
+new_list = []
+for elem in a:
+    new_list.append(elem)
+    a.remove(elem)
+print new_list
+
+a = ['1', 1, 0.1, 1, '01', '1', 10, '010', '10', [
+    10, 20], [20, 10], 1, [10, 20], ['20', '10']]
+new_list = []
+for elem in a:
+    new_list.append(elem)
+    a.remove(elem)
+print new_list
+
+a = [-78, 60, -24, -77, 47, 20, 78, -73, 63, -56, 20, 89, 66, -44, 28, 64, 96, 60, 21, -42, 92, -7, -53, 42, -2, 37, -85, 32, -57, -35, -93, -30, -36, -78, 8, -47, -32, 28, 50, 44, -76, -63, 80, 47, 21, -90, 48, 9, -61,
+     26, -11, -89, 84, 4, -39, -82, -80, -27, -15, -8, 5, 62, -79, 19, 34, -30, 11, 72, 82, -72, -49, -75, -71, -31, 24, 36, -33, -1, -95, -78, -9, 96, 88, 82, 77, 93, -96, -52, 80, 77, 95, 57, -48, 14, -40, -81, -81, 97, 29, -27]
+new_list = []
+for elem in a:
+    new_list.append(elem)
+    a.remove(elem)
+print new_list
+
 
 # Write a program that takes a string
-# a=”Special cases aren't special enough to break the rules.”
-# a=”Python was created in the early 1990s by Guido van Rossum at Stichting Mathematisch Centrum in the Netherlands as a successor of a language called ABC. Guido remains Python’s principal author, although it includes many contributions from others.”
+# a="Special cases aren't special enough to break the rules."
+# a="Python was created in the early 1990s by Guido van Rossum at Stichting Mathematisch Centrum in the Netherlands as a successor of a language called ABC. Guido remains Python's principal author, although it includes many contributions from others."
 # 	and returns the same string, except with the words in backwards order.
 
-a="Special cases aren't special enough to break the rules."
-a="Python was created in the early 1990s by Guido van Rossum at Stichting Mathematisch Centrum in the Netherlands as a successor of a language called ABC. Guido remains Python’s principal author, although it includes many contributions from others."
- 
+a = "Special cases aren't special enough to break the rules."
+my_list = a.split(' ')
+my_reversed_string = ''
+for elem in my_list:
+    my_reversed_string = elem+' '+my_reversed_string
+print (my_reversed_string)
+
+
+a = "Python was created in the early 1990s by Guido van Rossum at Stichting Mathematisch Centrum in the Netherlands as a successor of a language called ABC. Guido remains Python's principal author, although it includes many contributions from others."
+my_list = a.split(' ')
+my_reversed_string = ''
+for elem in my_list:
+    my_reversed_string = elem+' '+my_reversed_string
+print (my_reversed_string)
